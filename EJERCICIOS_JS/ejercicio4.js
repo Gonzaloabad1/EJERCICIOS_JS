777c1c5818cd94c8a226ca011c52710201d5f50b
Gonzalo Abad <135691252+Gonzaloabad1@users.noreply.github.com>
2023-06-15 11:55:02 +0200

    Delete EJERCICIOS_JS directory

--- EJERCICIOS_JS/ejercicio4.js
+++
@@ -1,9 +0,0 @@
-//1.1 
-let avengers = ["HULK", "SPIDERMAN", "BLACK PANTHER"];
-let avenger = avengers[0]; 
-console.log(avenger) 
-//1.2 
-avengers.shift();
-console.log(avengers);
-avengers.unshift("IRONMAN");
-console.log(avengers);
